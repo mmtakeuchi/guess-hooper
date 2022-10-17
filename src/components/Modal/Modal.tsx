6f9d3f6e42af267ebcebe4c1c2804d1d52bb2d43
mmtakeuchi <mikemtaki@gmail.com>
2022-10-17 15:43:28 -0700

    complete modal

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,76 @@
+import React, { useEffect } from 'react';
+import './Modal.scss';
+
+interface Props {
+  setIsOpen: (isOpen: boolean) => void;
+}
+
+const Modal = ({ setIsOpen }: Props) => {
+  useEffect(() => {
+    const closeOnEscapeKeyDown = (e: KeyboardEvent) => {
+      if (e.code === 'Escape') {
+        setIsOpen(false);
+      }
+    };
+
+    window.addEventListener('keydown', closeOnEscapeKeyDown);
+
+    return () => window.removeEventListener('keydown', closeOnEscapeKeyDown);
+  }, [setIsOpen]);
+
+  return (
+    <div className="modal" onClick={() => setIsOpen(false)}>
+      <div className="modal-box" onClick={(e) => e.stopPropagation()}>
+        <button
+          className="closeBtn"
+          aria-label="close"
+          onClick={() => setIsOpen(false)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-x"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            stroke-width="2"
+            stroke="currentColor"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <line x1="18" y1="6" x2="6" y2="18"></line>
+            <line x1="6" y1="6" x2="18" y2="18"></line>
+          </svg>
+        </button>
+        <div className="modal-box-content">
+          <h2>How To Play</h2>
+          <h3>Guess the NBA player in 6 tries.</h3>
+          <ul>
+            <li>
+              <span className="green">Green</span> in any column indicates a
+              match!
+            </li>
+            <li>
+              <span className="yellow">Yellow</span> in the team column
+              indicates the mystery player at one point played for this team,
+              but does not currently
+            </li>
+            <li>
+              <span className="yellow">Yellow</span> in the position column
+              indicates a partial match to the mystery player's position
+            </li>
+            <li>
+              <span className="yellow">Yellow</span> in any other column
+              indicates this attribute is within 2 (inches, years, numbers) of
+              the mystery player
+            </li>
+          </ul>
+          <p>A new player is released every day.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from '../Modal/Modal';
 import './Navbar.scss';
 
-interface Props {}
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-const Navbar = (props: Props) => {
-  return <header>Guess Hooper</header>;
+  return (
+    <header>
+      <h1>Guess Hooper</h1>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="icon icon-tabler icon-tabler-help"
+        width="32"
+        height="32"
+        viewBox="0 0 24 24"
+        stroke-width="2"
+        stroke="currentColor"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        onClick={() => setIsOpen(true)}
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <circle cx="12" cy="12" r="9"></circle>
+        <line x1="12" y1="17" x2="12" y2="17.01"></line>
+        <path d="M12 13.5a1.5 1.5 0 0 1 1 -1.5a2.6 2.6 0 1 0 -3 -4"></path>
+      </svg>
+      {isOpen && <Modal setIsOpen={setIsOpen} />}
+    </header>
+  );
 };
 
 export default Navbar;
