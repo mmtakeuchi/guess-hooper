da67f57bb1f521da60cf70a8ae2fa033ed1a2530
mmtakeuchi <mikemtaki@gmail.com>
2022-10-17 09:46:16 -0700

    create basic header and footer layouts

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.scss';
+
+interface Props {}
+
+const Footer = (props: Props) => {
+  return <footer>Github</footer>;
+};
+
+export default Footer;
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Navbar.scss';
+
+interface Props {}
+
+const Navbar = (props: Props) => {
+  return <header>Guess Hooper</header>;
+};
+
+export default Navbar;
